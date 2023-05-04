9b3c0ab22658616aff32965143c086195a1a63a5
Ksu <b1.shchedrina.oksana@gmail.com>
2023-05-04 14:49:22 +0200

    Update dz-7 according to comments

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        double distanceSun = 57.89; //віддаленість від сонця до меркурія
+//        double distanceSun1 = 57.89; //віддаленість від сонця до меркурія
 
         for (Planets planet: Planets.values()) {
-            distanceSun += planet.getDistanceSun();
-            System.out.println(planet.getNum() + " " + planet + " Віддаленість від сонця: " + distanceSun);
+//            distanceSun1 += planet.getDistanceSun();
+            System.out.println(planet.getNum() + " " + planet + " Віддаленість від сонця: " + planet.getDistanceSun() + " Наступна планета: " + planet.getNextPlanet());
         }
     }
 }
--- src/Planets.java
+++ src/Planets.java
@@ -1,51 +1,51 @@
 public enum Planets {
-    MERCURY(1, 0.0, 2439.7, "", "VENUS"),
-    VENUS(2, 50.27, 6051.8, "MERCURY", "EARTH"),
-    EARTH(3, 41.44, 6378.14, "VENUS", "MARS"),
-    MARS(4, 78.39, 3397, "EARTH", "JUPITER"),
-    JUPITER(5, 550.37, 71492, "MARS", "SATURN"),
-    SATURN(6, 648.65, 60268, "JUPITER", "URANUS"),
-    URANUS(7, 1448.26, 25559, "SATURN", "NEPTUNE"),
-    NEPTUNE(8, 1629.12, 24764, "URANUS", "");
+    MERCURY(1, 0.0, 2439.7, null),
+    VENUS(2, 50.27, 6051.8, Planets.MERCURY),
+    EARTH(3, 41.44, 6378.14, Planets.VENUS),
+    MARS(4, 78.39, 3397, Planets.EARTH),
+    JUPITER(5, 550.37, 71492, Planets.MARS),
+    SATURN(6, 648.65, 60268, Planets.JUPITER),
+    URANUS(7, 1448.26, 25559, Planets.SATURN),
+    NEPTUNE(8, 1629.12, 24764, Planets.URANUS);
 
     private final int num; // порядковий номер від сонця
     private final double distancePreviousPlanet; // віддаленість від попередньої планети (для меркурія 0) - міліонів км.
     private double distanceSun;//віддаленість від сонця
+    private double distanceSunToMercury = 57.89; //віддаленість від сонця до меркурія
     private final double radius; //радіус - км
-    private final String previousPlanet; //попередня планета
-    private final String nextPlanet; //наступна планета
+//    private final String previousPlanet; //попередня планета
+//    private String nextPlanet; //наступна планета
 
-    Planets(int num, double distancePreviousPlanet, double radius, String previousPlanet, String nextPlanet) {
+    Planets(int num, double distancePreviousPlanet, double radius, Planets previousPlanet) {
         this.num = num;
         this.distancePreviousPlanet = distancePreviousPlanet;
         this.radius = radius;
-        this.previousPlanet = previousPlanet;
-        this.nextPlanet = nextPlanet;
-        this.distanceSun = distanceSun + this.distancePreviousPlanet;
+//        this.previousPlanet = previousPlanet;
+//        this.nextPlanet = nextPlanet;
+//        this.distanceSun = distanceSun + this.distancePreviousPlanet;
+        if (previousPlanet == null) {
+            this.distanceSun = distanceSunToMercury;
+        } else {
+            this.distanceSun = previousPlanet.distanceSun + distancePreviousPlanet;
+        }
     }
 
     public int getNum() {
         return num;
     }
 
-    public double getDistancePreviousPlanet() {
-        return distancePreviousPlanet;
-    }
-
-    public double getRadius() {
-        return radius;
-    }
-
-    public String getPreviousPlanet() {
-        return previousPlanet;
-    }
-
-    public String getNextPlanet() {
-        return nextPlanet;
-    }
-
     public double getDistanceSun() {
         return distanceSun;
+    }
+
+    public Planets getNextPlanet() {
+        Planets nextPlanet = null;
+        for (Planets planet: Planets.values()) {
+            if (planet.num == this.num + 1) {
+                nextPlanet = planet;
+            }
+        }
+        return nextPlanet;
     }
 }
 
